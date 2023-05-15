b05e966f1fd27d57fe9997e5bb1802c2215923b8
Shalini Menon <shalinimenon.sash@gmail.com>
2023-05-15 17:21:20 +0530

    initial commit

---
+++ .eslint.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ["plugin:vue/vue3-essential", "prettier"],
+    ruler:{
+        // override/add rules setting here such as:
+
+        "vue/no-unsend-vars": "error",
+    },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    useTabs: false,
+    printWidth: 80,
+    endOfLine: "auto",
+    singleQoute: true,
+    trailingComma:"es5",
+    bracketSpacing:true,
+    arrowPrarens:"always",
+}
